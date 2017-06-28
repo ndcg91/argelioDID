6f02355dce6298ef8430ef3271e13ba7a679748e
System Administrator <root@Noels-MacBook-Pro.local>
2017-06-28 23:40:56 +0200

    testing

--- index.js
+++ index.js
@@ -36,62 +36,115 @@ app.listen(3000, function () {
 
 });
 
-
-var clientLoaded = (err,client) => {
-  var playback = client.Playback();
-
+/*jshint node: true*/
+'use strict';
+ 
+var ari = require('ari-client');
+var util = require('util');
+ 
+ari.connect('http://localhost:8088', 'asterisk', 'asterisk', clientLoaded);
+ 
+// handler for client being loaded
+function clientLoaded (err, client) {
   if (err) {
     throw err;
   }
-  
-
-  client.channels.list(function(err, channels) {
-    if (!channels.length) {
-      console.log('No channels currently :-(');
-    } else {
-      console.log('Current channels:');
-      channels.forEach(function(channel) {
-        console.log(channel);
-      });
-    }
-  });
-  client.on('StasisStart', stasisStart);
-  client.on('StasisEnd', stasisEnd);
-  client.start('did-api');
-
+ 
+  // handler for StasisStart event
   function stasisStart(event, channel) {
     console.log(util.format(
-        'Channel %s has entered the application', channel.name));
-
-    // use keys on event since channel will also contain channel operations
-    Object.keys(event.channel).forEach(function(key) {
-      console.log(util.format('%s: %s', key, JSON.stringify(channel[key])));
-    });
-
-    channel.play({media: 'sound:lots-o-monkeys'},playback, function(err, newPlayback) {
+          'Monkeys! Attack %s!', channel.name));
+ 
+    var playback = client.Playback();
+    channel.play({media: 'sound:lots-o-monkeys'},
+                  playback, function(err, newPlayback) {
       if (err) {
         throw err;
       }
     });
-
-    playback.on('PlaybackFinished', function(event, completedPlayback) {
+    playback.on('PlaybackFinished', playbackFinished);
+ 
+    function playbackFinished(event, completedPlayback) {
       console.log(util.format(
-        'Monkeys successfully vanquished %s; hanging them up',
-        channel.name));
-        channel.play({media: 'sound:lots-o-monkeys'},playback, function(err, newPlayback) {
-          if (err) {
-            throw err;
-          }
-        });
-       
-    });
-
+          'Monkeys successfully vanquished %s; hanging them up',
+          channel.name));
+      channel.play({media: 'sound:lots-o-monkeys'},
+                  playback, function(err, newPlayback) {
+        if (err) {
+          throw err;
+        }
+      });
+        
+    }
   }
-
+ 
+  // handler for StasisEnd event
   function stasisEnd(event, channel) {
-    console.log("end")
+    console.log(util.format(
+          'Channel %s just left our application', channel.name));
   }
+ 
+  client.on('StasisStart', stasisStart);
+  client.on('StasisEnd', stasisEnd);
+ 
+  client.start('channel-playback-monkeys');
 }
+
+// var clientLoaded = (err,client) => {
+//   var playback = client.Playback();
+
+//   if (err) {
+//     throw err;
+//   }
+  
+
+//   client.channels.list(function(err, channels) {
+//     if (!channels.length) {
+//       console.log('No channels currently :-(');
+//     } else {
+//       console.log('Current channels:');
+//       channels.forEach(function(channel) {
+//         console.log(channel);
+//       });
+//     }
+//   });
+//   client.on('StasisStart', stasisStart);
+//   client.on('StasisEnd', stasisEnd);
+//   client.start('did-api');
+
+//   function stasisStart(event, channel) {
+//     console.log(util.format(
+//         'Channel %s has entered the application', channel.name));
+
+//     // use keys on event since channel will also contain channel operations
+//     Object.keys(event.channel).forEach(function(key) {
+//       console.log(util.format('%s: %s', key, JSON.stringify(channel[key])));
+//     });
+
+//     channel.play({media: 'sound:lots-o-monkeys'},playback, function(err, newPlayback) {
+//       if (err) {
+//         throw err;
+//       }
+//     });
+
+//     playback.on('PlaybackFinished', function(event, completedPlayback) {
+//       console.log(util.format(
+//         'Monkeys successfully vanquished %s; hanging them up',
+//         channel.name));
+//         channel.play({media: 'sound:lots-o-monkeys'},playback, function(err, newPlayback) {
+//           if (err) {
+//             throw err;
+//           }
+//         });
+       
+//     });
+
+//   }
+
+//   function stasisEnd(event, channel) {
+//     console.log("end")
+//   }
+// }
 
 
 // handler for StasisStart event
